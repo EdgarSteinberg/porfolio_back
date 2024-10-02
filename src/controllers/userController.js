3e6361d8b2e7bad26c66cb4697f41368218c984f
¨Edgar <s.steinberg2019@gmail.com>
2024-10-02 14:33:18 -0300

    modified email

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -132,7 +132,7 @@ class UsersContoller {
             <h1>Solicitud de Recuperación de Contraseña</h1>
             <p>Hemos recibido una solicitud para restablecer tu contraseña. Si no realizaste esta solicitud, por favor ignora este correo.</p>
             <p>Para restablecer tu contraseña, haz clic en el siguiente enlace:</p>
-            <a href="https://mi-frontend-en-produccion.com/reset-password?token=${token}">Restablecer contraseña</a>
+            <a href="https://porfolio-frontend.onrender.com/reset-password?token=${token}">
             <button class="btnChat">Restablecer Contraseña</button>
             </a>
             <p>Este enlace es válido por 1 hora.</p>
